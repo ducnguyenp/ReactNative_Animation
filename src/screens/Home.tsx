43c388e9121f1b3b8dee3771d8bbc469e375e55e
ducnguyenp <duc.np@iparamed.com>
2023-05-14 14:32:32 +0700

    Add styled-component and Tailwind css

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,8 +1,12 @@
-import React from 'react'
-import { Text, View } from 'react-native'
+import React from "react";
+import { Text, View } from "react-native";
 
 const Home = () => {
-    return <View><Text>This is HomeScreen</Text></View>
-}
+  return (
+    <View className="flex-1 items-center justify-center bg-slate-800">
+      <Text className="text-lg">This is HomeS444creen</Text>
+    </View>
+  );
+};
 
-export default Home
+export default Home;
